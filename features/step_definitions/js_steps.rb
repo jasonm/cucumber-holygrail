6943c834c8559ceac17eccbb6cdf69bf18baa95b
Jason Morrison <jmorrison@thoughtbot.com>
2010-03-13 19:15:46 -0500

    Refactor cucumber_js helper method

--- features/step_definitions/js_steps.rb
+++ features/step_definitions/js_steps.rb
@@ -1,18 +1,16 @@
 def cucumber_js(code)
   HolyGrail::XhrProxy.context = self
   @__page ||= Harmony::Page.new(HolyGrail::Assertions::XHR_MOCK_SCRIPT + rewrite_script_paths(@response.body.to_s))
-  @__page.execute_js(code)
-end
+  @__javascript_result = @__page.execute_js(code)
 
-When /^I run the following javascript:$/ do |src|
-  @_javascript_result = cucumber_js(src)
-
-  page_with_xhr_mock_script = @__page.to_html
-  page_without_xhr_mock_script = page_with_xhr_mock_script.gsub(HolyGrail::Assertions::XHR_MOCK_SCRIPT.strip, '')
-
+  page_without_xhr_mock_script = @__page.to_html.gsub(HolyGrail::Assertions::XHR_MOCK_SCRIPT.strip, '')
   response.body = page_without_xhr_mock_script
 end
 
+When /^I run the following javascript:$/ do |src|
+  cucumber_js(src)
+end
+
 Then /^the result of that javascript should be "([^\"]*)"$/ do |value|
-  assert_equal value, @_javascript_result.to_s
+  assert_equal value, @__javascript_result.to_s
 end
